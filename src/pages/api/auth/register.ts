e9d0698361943a92ae7c75071bbfdd5ce59170fe
mariaescalante <marescram3@alum.us.es>
2024-02-07 13:19:17 +0100

    formulario de registro mejorado

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -5,9 +5,13 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
+  const nombre = formData.get("nombre")?.toString();
+  const apellidos = formData.get("apellidos")?.toString();
+  const telefono = formData.get("telefono")?.toString();
 
-  if (!email || !password) {
-    return new Response("Email and password are required", { status: 400 });
+
+  if (!email || !password || !nombre || !apellidos || !telefono) {
+    return new Response("Debe rellenar todos los campos", { status: 400 });
   }
 
   const { error } = await supabase.auth.signUp({
@@ -19,5 +23,17 @@ export const POST: APIRoute = async ({ request, redirect }) => {
     return new Response(error.message, { status: 500 });
   }
 
-  return redirect("/userData");
+  if (error == null) {
+    const { data, error } = await supabase
+      .from('Usuario')
+      .insert([
+        { nombre: nombre, apellidos: apellidos, email: email, telefono: telefono },
+      ]).select()
+
+    if (error != null && error['message'] == 'duplicate key value violates unique constraint "usuarios_email_key"') {
+      return new Response("Este email ya está registrado", { status: 400 });
+    }
+  }
+
+  return redirect("/confirmEmail");
 };
