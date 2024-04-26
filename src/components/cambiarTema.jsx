d285d41973e6e44f0721b541b0b88af24f3a7476
mariaescalante <marescram3@alum.us.es>
2024-04-26 19:55:02 +0200

    Switch dark mode

--- src/components/cambiarTema.jsx
+++ src/components/cambiarTema.jsx
@@ -22,14 +22,15 @@ function cambiarTema() {
 
   return (
     <div>
-      <button
-        className="hover:scale-125 transition-all hover:contrast-125"
-        onClick={handleChangeTheme}
-      >
-        {theme === 'light' ? <i className="fas fa-moon"></i> : <i className="fas fa-sun"></i>}
-      </button>
+      <label className="inline-flex items-center me-5 cursor-pointer">
+        <i className={theme === 'dark' ? "fas fa-sun" : "fas fa-moon"} />
+        <input type="checkbox" value="" className="sr-only peer" checked={theme === 'dark'} onChange={handleChangeTheme} />
+        <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-red-300 dark:peer-focus:ring-red-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-tela-500 ml-2"></div>
+      </label>
     </div>
   );
+
+
 }
 
 export default cambiarTema;
